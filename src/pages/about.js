1fc4b3fc8dea16843d59bfd887be583507b684c8
zzeris <zzeris@gmail.com>
2019-11-06 15:58:21 -0300

    Add layout pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return(
-        <div>
-            <Header />
+        <Layout>
             <h1>Quem é José Luiz?</h1>
             <p>Desenvolvedor full cycle, atualmente desenvolvendo em NodeJS e ReactJS.</p>
             <p><Link to="/contact">Quer trabalhar comigo?</Link></p>
-            <Footer />
-        </div>
+        </Layout>
     )
 }
 
