e1a2fd8bd3bba670a9f903b0309d0dc9896a767f
zzeris <zzeris@gmail.com>
2019-11-13 10:24:13 -0300

    Add siteMetadata graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => {
     return (
         <Layout>
-            <h1>Olá!</h1>
+            <h1>Olá! Me chamo José Luiz.</h1>
             <Link to="/about">Sobre mim.</Link>
         </Layout>
     )
